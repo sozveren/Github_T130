35c0cbc4e07be282485f6b6dcd9f99755d6cd687
Sedat Ozveren <sedatozveren@gmail.com>
2023-12-09 21:08:49 +0300

    2. gün ilk version

---
+++ src/firstGitHub/Gun.java
@@ -0,0 +1,9 @@
+package firstGitHub;
+
+public class Gun {
+    public static void main(String[] args) {
+         int a = 120;
+         int b = 256;
+        System.out.println("İki sayının toplamı : " + (a+b));
+    }
+}
